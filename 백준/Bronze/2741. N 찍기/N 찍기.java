0c7ae080997c39b10f26989105f7dc0b8afd67f8
tk.yoon <126501685+YOONTAEKYUN@users.noreply.github.com>
2023-10-04 17:39:54 +0900

    [Bronze V] Title: N 찍기, Time: 1100 ms, Memory: 31264 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int n = Integer.parseInt(input);
+        for (int i =1; i<n+1; i++){
+            System.out.println(i);
+        }
+        br.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
