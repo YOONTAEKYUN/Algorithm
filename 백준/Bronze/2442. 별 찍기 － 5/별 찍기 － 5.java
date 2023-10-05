d072724376139abe9a8698e381c3dcc7143dd301
tk.yoon <tk.yoon0105@gmail.com>
2023-10-05 13:43:14 +0900

    별 찍기 -5 repeat 방식으로 수정

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -6,13 +6,7 @@ public class Main {
         int n = scanner.nextInt();
 
         for (int i = 1; i< n + 1; i++){
-            for (int j = 0; j< n-i; j++){
-                    System.out.print(" ");
-            }
-            for (int j = 0; j< 2 * i - 1; j++){
-                System.out.print("*");
-            }
-            System.out.println("");
+            System.out.println(" ".repeat(n-i)+"*".repeat(2*i-1));
         }
     }
 
