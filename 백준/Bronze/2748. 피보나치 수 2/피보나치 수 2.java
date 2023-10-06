9d710715fef3d913517f41f0eb7e34a68de7df5b
tk.yoon <126501685+YOONTAEKYUN@users.noreply.github.com>
2023-10-06 15:21:47 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int num = 0;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(fibonacci(n));
+        System.out.println(sb);
+        br.close();
+    }
+
+    private long fibonacci(int n) {
+        long sum = 1;
+        long f1 = 0;
+        long f2 = 1;
+
+        for(int i = 1 ; i < n ; i++) {
+            sum = f1 + f2;
+            f1 = f2;
+            f2 = sum;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
