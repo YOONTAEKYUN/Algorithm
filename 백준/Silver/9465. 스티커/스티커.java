f8a4faa5b434971285ef23e194264711bb6f74af
tk.yoon <126501685+YOONTAEKYUN@users.noreply.github.com>
2023-10-18 18:53:34 -0700

    [Silver I] Title: 스티커, Time: 620 ms, Memory: 130444 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,32 +1,39 @@
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Main {
-    public static void main(String[] args) throws IOException {
+public class Main {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] result = new int[n];
 
-        int T = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < T; i++) {
-            int n = Integer.parseInt(br.readLine());
-            String[] input = new String[n];
-            int[][] arr = new int[2][n + 1];
-            int[][] dp = new int[2][n + 1];
-            for (int j = 0; j < 2; j++) { //초기화
-                input = br.readLine().split(" ");
-                for (int k = 1; k <= n; k++) {
-                    arr[j][k] = Integer.parseInt(input[k-1]);
+        for (int i = 0; i < n; i++){
+            int colNum = Integer.parseInt(br.readLine());
+            int[] dp0 = new int[colNum]; int[] dp1 = new int[colNum];
+            int[][]  stickerSet = new int[2][colNum];
+            for (int j = 0; j < 2; j++){
+                String[] input = br.readLine().split(" ");
+                for (int k = 0; k < colNum; k++){
+                    stickerSet[j][k] = Integer.parseInt(input[k]);
+                    if (j == 1){
+                        if (k == 0){
+                            dp0[k] = stickerSet[0][k];
+                            dp1[k] = stickerSet[1][k];
+                        } else if (k == 1){
+                            dp0[k] = dp1[k-1] + stickerSet[0][k]; dp1[k] = dp0[k-1] + stickerSet[1][k];
+                        } else {
+                            dp0[k] = Math.max(dp1[k-2], dp1[k-1]) + stickerSet[0][k];
+                            dp1[k] = Math.max(dp0[k-2], dp0[k-1]) + stickerSet[1][k];
+                        }
+                    }
                 }
             }
-            dp[0][1] = arr[0][1];
-            dp[1][1] = arr[1][1];
-            for (int a = 2; a <= n; a++) {
-                dp[0][a] = Math.max(dp[1][a - 1], dp[1][a - 2]) + arr[0][a];
-                dp[1][a] = Math.max(dp[0][a - 1], dp[0][a - 2]) + arr[1][a];
-            }
-            System.out.println(Math.max(dp[0][n], dp[1][n]));
+            result[i] =(Math.max(dp0[colNum-1],dp1[colNum-1]));
         }
+        for (int i = 0; i< n; i++){
+            System.out.println(result[i]);
+        }
+        br.close();
     }
 }
