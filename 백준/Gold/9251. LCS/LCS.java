720060969b59046f6f0b9bc8d186951b32268e66
윤태균 <126501685+taekyun-yoon@users.noreply.github.com>
2024-01-11 20:48:25 +0900

    [Gold V] Title: LCS, Time: 184 ms, Memory: 19276 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -10,18 +10,17 @@ public class Main {
         String[] listA = a.split("");
         String[] listB = b.split("");
 
-        int[][] dp = new int[listB.length + 1][listA.length + 1];
+        int[][] dp = new  int[listB.length + 1][listA.length + 1];
 
         for (int i = 1; i <= listB.length; i++) {
             for (int j = 1; j <= listA.length; j++) {
                 if (listA[j-1].charAt(0) == listB[i-1].charAt(0)) {
-                    dp[i][j] = dp[i-1][j-1] + 1;
+                    dp[i][j] = dp[i-1][j - 1] + 1;
                 } else {
-                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
                 }
             }
         }
-
         bw.write(String.valueOf(dp[listB.length][listA.length]));
         bw.close();
         br.close();
