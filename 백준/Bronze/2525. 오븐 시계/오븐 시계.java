51012131882df6a466204386cffa982bc2350219
윤태균 <126501685+taekyun-yoon@users.noreply.github.com>
2024-04-15 00:12:14 +0900

    [Bronze III] Title: 오븐 시계, Time: 156 ms, Memory: 16076 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,49 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int out_h = 0;
+        int out_m = 0;
+
+        String input = br.readLine();
+        String[] inputList = input.split(" ");
+
+        int input_time = Integer.parseInt(br.readLine());
+
+        int h = Integer.parseInt(inputList[0]);
+        int m = Integer.parseInt(inputList[1]);
+
+        int add_h = input_time / 60;
+        int add_m = input_time % 60;
+
+        if (add_m + m >= 60) {
+            out_h = h + add_h + 1;
+            if (out_h >= 24) {
+                int temp = out_h;
+                out_h = temp - 24;
+            }
+            out_m = m + add_m - 60;
+        } else {
+            out_h = h + add_h;
+            if (out_h >= 24) {
+                int temp = out_h;
+                out_h = temp - 24;
+            }
+            out_m = m + add_m;
+        }
+
+        // System.out.print(out_h);
+        // System.out.print(" ");
+        // System.out.println(out_m);
+
+        bw.write(out_h + " " + out_m + "\n");
+        bw.flush();
+        bw.close();
+
+        br.close();
+    }
+}
