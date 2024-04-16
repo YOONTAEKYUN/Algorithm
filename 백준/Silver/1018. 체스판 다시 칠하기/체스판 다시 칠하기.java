43ffe800a969cff61dd405003595f49707c48022
윤태균 <126501685+taekyun-yoon@users.noreply.github.com>
2024-04-16 19:20:49 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 124 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -2,103 +2,57 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // ex) 10 * 13
+
+        // 보드의 크기 N과 M
         String[] NM = br.readLine().split(" ");
-        // row 10
         int N = Integer.parseInt(NM[0]);
-        // column 13
         int M = Integer.parseInt(NM[1]);
 
-        String[][] chessboard = new String[N][M];
-
-        // 현재 빈도수
-        int tempResult = 0;
-        // 최소 빈도수
-        int result = Integer.MAX_VALUE;
-
+        // 보드 입력 받기
+        char[][] board = new char[N][M];
         for (int i = 0; i < N; i++) {
-            chessboard[i] = br.readLine().split("");
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                board[i][j] = line.charAt(j);
+            }
         }
 
-        // ex) 10 * 13: 첫 왼쪽 위(기준)의 범위 [0][0]~[2][5]
-        // 8x8 chessboard cutting
-        for (int i = 0; i < N - 7; i++) {
-            for (int j = 0; j < M - 7; j++) {
-                // 체스판 고정
-                tempResult = 0;
-                // System.out.printf("start index point [%d][%d]: \n", i, j);
-                // chessboard[i][j] 체스판 첫 왼쪽 위 색(기준) (경우의 수 둘다 구해서 대소 비교필)
-                // if (chessboard[i][j].equals("B"))
-                {
-                    for (int k = 0; k < 8; k++) {
-                        for (int z = 0; z < 8; z++) {
-                            // System.out.print(chessboard[i + k][j + z] + " ");
-                            // [홀수][홀수] or [짝수][짝수] -> b
-                            if ((i + k) % 2 != 0 & (j + z) % 2 != 0 | ((i + k) % 2 == 0 & (j + z) % 2 == 0)) {
-                                if (chessboard[i + k][j + z].equals("B")) {
-                                    // System.out.print("0 ");
-                                } else {
-                                    tempResult += 1;
-                                    // System.out.print("1 ");
-                                }
-                            } else { // 나머지 -> w
-                                if (chessboard[i + k][j + z].equals("W")) {
-                                    // System.out.print("0 ");
-                                } else {
-                                    tempResult += 1;
-                                    // System.out.print("1 ");
-                                }
-                            }
-                        }
-                        // System.out.println();
+        int minCount = Integer.MAX_VALUE;
+
+        // 8x8 체스판을 이동하며 최소 다시 칠해야 하는 정사각형의 개수 계산
+        for (int i = 0; i <= N - 8; i++) {
+            for (int j = 0; j <= M - 8; j++) {
+                minCount = Math.min(minCount, calculateRepaintCount(board, i, j));
+            }
+        }
+
+        System.out.println(minCount);
+
+        br.close();
+    }
+
+    // 주어진 시작 위치에서 8x8 체스판을 탐색하여 최소 다시 칠해야 하는 정사각형의 개수 계산
+    private static int calculateRepaintCount(char[][] board, int startX, int startY) {
+        int repaintCount = 0;
+
+        // 시작 위치에서 8x8 체스판을 탐색
+        for (int i = startX; i < startX + 8; i++) {
+            for (int j = startY; j < startY + 8; j++) {
+                // 현재 칸의 색과 기대되는 색이 다른 경우 다시 칠해야 함
+                if ((i + j) % 2 == 0) { // 행 번호와 열 번호의 합이 짝수인 경우
+                    if (board[i][j] != 'W') {
+                        repaintCount++;
                     }
-                }
-
-                if (tempResult < result) {
-                    result = tempResult;
-                }
-                tempResult = 0;
-                // else if (chessboard[i][j].equals("W"))
-                {
-                    for (int k = 0; k < 8; k++) {
-                        for (int z = 0; z < 8; z++) {
-                            // System.out.print(chessboard[i + k][j + z] + " ");
-                            // [홀수][홀수] or [짝수][짝수] -> w
-                            if ((i + k) % 2 != 0 & (j + z) % 2 != 0 | ((i + k) % 2 == 0 & (j + z) % 2 == 0)) {
-                                if (chessboard[i + k][j + z].equals("W")) {
-                                    // System.out.print("0 ");
-                                } else {
-                                    tempResult += 1;
-                                    // System.out.print("1 ");
-                                }
-                            } else { // 나머지 -> b
-                                if (chessboard[i + k][j + z].equals("B")) {
-                                    // System.out.print("0 ");
-
-                                } else {
-                                    tempResult += 1;
-                                    // System.out.print("1 ");
-                                }
-                            }
-                        }
-                        // System.out.println();
+                } else { // 행 번호와 열 번호의 합이 홀수인 경우
+                    if (board[i][j] != 'B') {
+                        repaintCount++;
                     }
-                }
-
-                // System.out.printf("tempResult: %d || result: %d \n\n", tempResult, result);
-                // 현재 체스 판(8x8)에서 칠할 횟수: tempResult 비교
-                if (tempResult < result) {
-                    result = tempResult;
                 }
             }
         }
-        System.out.println(result);
-        // bw.write("\n");
-        // bw.flush();
-        // bw.close();
 
-        br.close();
+        // 해당 체스판을 흰색으로 시작했을 때와 검은색으로 시작했을 때 중에서 최소값 반환
+        return Math.min(repaintCount, 64 - repaintCount);
     }
 }
