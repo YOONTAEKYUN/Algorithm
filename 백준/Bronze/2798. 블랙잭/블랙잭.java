dfe9dc49fc71b4be94b35921c8ad0bb15c3f327c
윤태균 <126501685+taekyun-yoon@users.noreply.github.com>
2024-04-16 15:23:53 +0900

    [Bronze II] Title: 블랙잭, Time: 132 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] NM = br.readLine().split(" ");
+        int N = Integer.parseInt(NM[0]);
+        int M = Integer.parseInt(NM[1]);
+
+        String[] inputString = br.readLine().split(" ");
+        int[] input = Arrays.asList(inputString).stream().mapToInt(Integer::parseInt).toArray();
+
+        int tempResult = 0;
+
+        for (int i = 0; i < input.length - 2; i++) {
+            for (int j = i + 1; j < input.length - 1; j++) {
+                for (int k = j + 1; k < input.length; k++) {
+                    int currentTotal = input[i] + input[j] + input[k];
+                    if (currentTotal <= M) {
+                        if (currentTotal > tempResult) {
+                            tempResult = currentTotal;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(tempResult);
+        br.close();
+    }
+}
