1ca57e12b258ab13c3abd68987b5c05b07389226
윤태균 <126501685+taekyun-yoon@users.noreply.github.com>
2024-04-20 17:39:23 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 144 ms, Memory: 15876 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -5,22 +5,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         String S = br.readLine();
 
-        String az = "abcdefghijklmnopqrstuvwxyz";
-        int[] result = new int[az.length()];
-        Arrays.fill(result, -1);
-        for (int i = 0; i < S.length(); i++) {
-            for (int j = 0; j < az.length(); j++) {
-                if (S.charAt(i) == az.charAt(j) & result[j] == -1) {
-                    result[j] = i;
-                }
-            }
-            S.charAt(i);
+        for (char i = 'a'; i <= 'z'; i++) {
+            sb.append(S.indexOf(i) + " ");
         }
-        for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i] + " ");
-        }
+        System.out.println(sb);
 
         br.close();
     }
