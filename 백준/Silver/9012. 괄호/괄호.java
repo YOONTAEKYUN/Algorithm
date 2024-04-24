e3ab854629afde0bdddc241c018ddbda6bc7b98c
윤태균 <126501685+taekyun-yoon@users.noreply.github.com>
2024-04-24 23:09:17 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -7,57 +7,17 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int T = Integer.parseInt(br.readLine());
-        StringBuilder result = new StringBuilder();
-        int lps = 0;
-        int rps = 0;
-        int ps = 0;
+        StringBuffer sb = new StringBuffer();
 
         for (int i = 0; i < T; i++) {
-            lps = 0;
-            rps = 0;
-            ps = 0;
-            
-            String[] input = br.readLine().split("");
-
-            for (int j = 0; j < input.length; j++) {
-                if (ps == 0 ) {
-                    if (input[j].equals("(")) {
-                        lps += 1;
-                        ps += 1;
-                    }else{
-                        rps += 1;
-                        break;
-                    }
-                }else if (ps < 0) {
-                    if (input[j].equals("(")) {
-                        lps += 1;
-                    }else{
-                        rps += 1;
-                    }
-                    break;
-                }
-                else{
-                    if (input[j].equals("(")) {
-                        lps += 1;
-                        ps += 1;
-                    } else if (input[j].equals(")")) {
-                        rps += 1;
-                        ps -= 1;
-                    }
-                }
+            String input = br.readLine();
+            while (input.contains("()")) {
+                input = input.replace("()", "");
             }
-        
-            if (lps == rps & ps == 0) {
-                result.append("YES");
-            }else{
-                result.append("NO");
-            }
-            result.append("\n");
+            sb.append(input.equals("")? "YES\n" : "NO\n");
         }
-        System.out.println(result);
-
+       
+        System.out.println(sb);
         br.close();
-
 	}
-
 }
