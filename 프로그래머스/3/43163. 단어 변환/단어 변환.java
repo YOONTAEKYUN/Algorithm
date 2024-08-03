0b1f49555280a913a6adef387addfbaed103f8b9
윤태균 <126501685+taekyun-yoon@users.noreply.github.com>
2024-08-03 14:15:19 +0900

    [level 3] Title: 단어 변환, Time: 1.32 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,35 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
+        int answer = 0;
         Set<String> wordSet = new HashSet<>(Arrays.asList(words));
+        Map<String, Integer> checkMap = new HashMap<>();
+        Queue<String> q = new LinkedList<>();
         
-        if (!wordSet.contains(target)) return 0;
+        q.offer(begin);
+        checkMap.put(begin, 0);
         
-        Queue<String> queue = new LinkedList<>();
-        Map<String, Integer> distance = new HashMap<>();
-        
-        queue.offer(begin);
-        distance.put(begin, 0);
-        
-        while (!queue.isEmpty()) {
-            String current = queue.poll();
-            if (current.equals(target)) {
-                return distance.get(current);
+        while(!q.isEmpty()) {
+            String current = q.poll();
+            int size = current.length();
+            
+            if(current.equals(target)) {
+                answer = checkMap.get(current);
+                break;
             }
             
-            for (int i = 0; i < current.length(); i++) {
-                char[] chars = current.toCharArray();
-                for (char c = 'a'; c <= 'z'; c++) {
-                    chars[i] = c;
-                    String next = new String(chars);
-                    if (wordSet.contains(next) && !distance.containsKey(next)) {
-                        queue.offer(next);
-                        distance.put(next, distance.get(current) + 1);
+            for(int i = 0; i < size; i++) {
+                char[] str = current.toCharArray();
+                for(char ch = 'a'; ch < 'z'; ch++) {
+                    str[i] = ch;
+                    String changed = new String(str);
+                    
+                    if(wordSet.contains(changed) && !checkMap.containsKey(changed)){
+                        q.offer(changed);
+                        checkMap.put(changed, checkMap.get(current) + 1);
                     }
                 }
             }
+            
         }
         
-        return 0;
+        return answer;
     }
 }
